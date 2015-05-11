fa2dbee63f99da1ff01201163bc56389a9100008
Chris Davies <chris.davies@theironyard.com>
2015-05-11 10:29:02 -0400

    Updating demo and linking to it for wk2 day1

--- demos/js101/js/js101.js
+++ demos/js101/js/js101.js
@@ -1,4 +1,4 @@
-// Get the canvas context
+// Canvas context
 var ctx = document
   .querySelector('canvas')
   .getContext('2d');
@@ -7,6 +7,29 @@ var ctx = document
 // the pixels
 ctx.translate(0.5, 0.5);
 
-//
-// TODO: Put all drawing logic and shtuff here...
-//
+// TODO: Put logic below this
+
+function drawSignalStrength (barX, barY) {
+  var barHeight = 25;
+  var barWidth = 15;
+  var maxHeight = 125;
+
+  // while (barHeight < maxHeight) {
+  // while (barHeight >= maxHeight) {
+  // while (barHeight == maxHeight) {
+  // while (barHeight != maxHeight) {
+  while (barHeight <= maxHeight) {
+    ctx.fillRect(
+      barX,
+      barY + maxHeight - barHeight,
+      barWidth,
+      barHeight);
+
+    barX += 25;
+    barHeight = barHeight + 25;
+  }
+}
+
+drawSignalStrength(0, 100);
+drawSignalStrength(150, 200);
+drawSignalStrength(300, 300);
