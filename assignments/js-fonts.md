be552a3a1deb78b5fa8e887055c1312a387e9f75
Chris Davies <chris.davies@theironyard.com>
2015-05-15 22:06:19 -0400

    Adding week3 stuffs

---
+++ assignments/js-fonts.md
@@ -0,0 +1,75 @@
+# JS Fonts
+
+## Description
+
+I've put together a dataset containing some metadata about common fonts.
+
+I got the data from [this handy site](http://www.cssfontstack.com/).
+
+Your job is to use JavaScript to calculate some statistics about these fonts,
+and also to render them as HTML!
+
+## Objectives
+
+### Learning Objectives
+
+After completing this assignment, you should understand...
+
+* The array map, filter, reduce, and forEach methods
+* Have a deeper understanding of hashes
+
+## Resources
+
+- [The font dataset](https://github.com/tiy-durham-fe-cohort4/resources/blob/master/dataset/fonts.js)
+
+## Details
+
+### Deliverables
+
+* A GitHub project called "js-fonts"
+
+## Normal Mode (MVP)
+
+- Use the array `reduce` method to calculate the average Mac and Win support
+across all fonts.
+- Use the array `filter` method to get only those fonts that have a 90% or
+higher Mac *and* Win support
+- Use the array `map` method to convert the list of fonts into a string of
+HTML. An example of the HTML you might want to produce is as follows:
+
+```
+
+<li>
+  <span class="family">Arial (sans-serif)</span>
+  <span class="stats">
+    <span class="win"><em>Windows</em> 99.84%</span>
+    <span class="mac"><em>Mac</em> 98.74%</span>
+  </span>
+</li>
+<li>
+  <span class="family">Arial Black (sans-serif)</span>
+  <span class="stats">
+    <span class="win"><em>Windows</em> 98.08%</span>
+    <span class="mac"><em>Mac</em> 96.22%</span>
+  </span>
+</li>
+
+...etc
+
+```
+
+- Use `innerHTML` or `insertAdjacentHTML` to stick your generated HTML into
+the DOM
+- Style it!!!
+
+## Hard Mode
+
+- Display the stats that you calculated in normal mode
+- When the user clicks a font, change the page (or a preview panel) to use that
+font family
+- Allow the user to enter in a mac percentage e.g. `90.5` and a PC percentage,
+then narrow down the fonts that are displayed to only show fonts that meet those
+minimum percentages
+- Generate a list of checkboxes for the various types of fonts (serif, sans-serif, monospaced, etc).
+  - When the user checks one or more checkboxes, show only the fonts that
+  match the checked font types
---
+++ datasets/fonts.js
@@ -0,0 +1,231 @@
+var fonts = [{
+  family: 'Arial',
+  win: 99.84,
+  mac: 98.74,
+  type: 'sans-serif'
+},{
+  family: 'Arial Black',
+  win: 98.08,
+  mac: 96.22,
+  type: 'sans-serif'
+},{
+  family: 'Arial Narrow',
+  win: 88.39,
+  mac: 94.77,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Arial Rounded MT Bold',
+  win: 59.53,
+  mac: 95.14,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Avant Garde',
+  win: 0,
+  mac: 1.08,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Calibri',
+  win: 83.41,
+  mac: 38.74,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Candara',
+  win: 83.08,
+  mac: 34.41,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Century Gothic',
+  win: 87.62,
+  mac: 53.15,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Franklin Gothic Medium',
+  win: 99.18,
+  mac: 2.1,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Futura',
+  win: 1.26,
+  mac: 94.41,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Geneva',
+  win: 2.08,
+  mac: 99.64,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Gill Sans',
+  win: 58.54,
+  mac: 95.5,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Helvetica',
+  win: 7.34,
+  mac: 100,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Impact',
+  win: 0,
+  mac: 95.14,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Lucida Grande',
+  win: 0,
+  mac: 100,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Optima',
+  win: 2.52,
+  mac: 93.69,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Segoe UI',
+  win: 75.36,
+  mac: 0,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Tahoma',
+  win: 99.95,
+  mac: 91.71,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Trebuchet MS',
+  win: 99.67,
+  mac: 97.12,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Verdana',
+  win: 99.84,
+  mac: 99.1,
+  type: 'sans-serif'
+},{
+  fontFamily: 'Big Caslon',
+  win: 0,
+  mac: 92.61,
+  type: 'serif'
+},{
+  fontFamily: 'Bodoni MT',
+  win: 55.81,
+  mac: 0,
+  type: 'serif'
+},{
+  fontFamily: 'Book Antiqua',
+  win: 86.09,
+  mac: 49.01,
+  type: 'serif'
+},{
+  fontFamily: 'Calisto MT',
+  win: 58.43,
+  mac: 46.31,
+  type: 'serif'
+},{
+  fontFamily: 'Cambria',
+  win: 83.35,
+  mac: 35.32,
+  type: 'serif'
+},{
+  fontFamily: 'Didot',
+  win: 0,
+  mac: 93.51,
+  type: 'serif'
+},{
+  fontFamily: 'Garamond',
+  win: 86.47,
+  mac: 49.91,
+  type: 'serif'
+},{
+  fontFamily: 'Georgia',
+  win: 99.4,
+  mac: 97.48,
+  type: 'serif'
+},{
+  fontFamily: 'Goudy Old Style',
+  win: 58.11,
+  mac: 47.57,
+  type: 'serif'
+},{
+  fontFamily: 'Hoefler Text',
+  win: 0.99,
+  mac: 92.61,
+  type: 'serif'
+},{
+  fontFamily: 'Lucida Bright',
+  win: 76.12,
+  mac: 99.64,
+  type: 'serif'
+},{
+  fontFamily: 'Palatino',
+  win: 99.29,
+  mac: 86.13,
+  type: 'serif'
+},{
+  fontFamily: 'Perpetua',
+  win: 66.54,
+  mac: 0,
+  type: 'serif'
+},{
+  fontFamily: 'Rockwell',
+  win: 65.94,
+  mac: 0,
+  type: 'serif'
+},{
+  fontFamily: 'Rockwell Extra Bold',
+  win: 66.1,
+  mac: 0,
+  type: 'serif'
+},{
+  fontFamily: 'Baskerville',
+  win: 60.35,
+  mac: 93.33,
+  type: 'serif'
+},{
+  fontFamily: 'Times New Roman',
+  win: 99.67,
+  mac: 97.48,
+  type: 'serif'
+},{
+  fontFamily: 'Consolas',
+  win: 82.97,
+  mac: 34.77,
+  type: 'monospaced'
+},{
+  fontFamily: 'Courier New',
+  win: 99.73,
+  mac: 95.68,
+  type: 'monospaced'
+},{
+  fontFamily: 'Lucida Console',
+  win: 99.18,
+  mac: 0,
+  type: 'monospaced'
+},{
+  fontFamily: 'Lucida Sans Typewriter',
+  win: 74.81,
+  mac: 99.64,
+  type: 'monospaced'
+},{
+  fontFamily: 'Monaco',
+  win: 2.74,
+  mac: 99.82,
+  type: 'monospaced'
+},{
+  fontFamily: 'Andale Mono',
+  win: 4.16,
+  mac: 94.59,
+  type: 'monospaced'
+},{
+  fontFamily: 'Copperplate',
+  win: 66.87,
+  mac: 92.61,
+  type: 'fantasy'
+},{
+  fontFamily: 'Papyrus',
+  win: 70.37,
+  mac: 92.43,
+  type: 'fantasy'
+},{
+  fontFamily: 'Brush Script MT',
+  win: 59.64,
+  mac: 90.99,
+  type: 'script'
+}];
