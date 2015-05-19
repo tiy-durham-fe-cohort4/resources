f29860a127d84f85df685f7c5cdb142dac8e3506
Chris Davies <chris.davies@theironyard.com>
2015-05-19 08:59:42 -0400

    Fixing bug in fonts.js

--- datasets/fonts.js
+++ datasets/fonts.js
@@ -14,217 +14,217 @@ var fonts = [{
   mac: 94.77,
   type: 'sans-serif'
 },{
-  fontFamily: 'Arial Rounded MT Bold',
+  family: 'Arial Rounded MT Bold',
   win: 59.53,
   mac: 95.14,
   type: 'sans-serif'
 },{
-  fontFamily: 'Avant Garde',
+  family: 'Avant Garde',
   win: 0,
   mac: 1.08,
   type: 'sans-serif'
 },{
-  fontFamily: 'Calibri',
+  family: 'Calibri',
   win: 83.41,
   mac: 38.74,
   type: 'sans-serif'
 },{
-  fontFamily: 'Candara',
+  family: 'Candara',
   win: 83.08,
   mac: 34.41,
   type: 'sans-serif'
 },{
-  fontFamily: 'Century Gothic',
+  family: 'Century Gothic',
   win: 87.62,
   mac: 53.15,
   type: 'sans-serif'
 },{
-  fontFamily: 'Franklin Gothic Medium',
+  family: 'Franklin Gothic Medium',
   win: 99.18,
   mac: 2.1,
   type: 'sans-serif'
 },{
-  fontFamily: 'Futura',
+  family: 'Futura',
   win: 1.26,
   mac: 94.41,
   type: 'sans-serif'
 },{
-  fontFamily: 'Geneva',
+  family: 'Geneva',
   win: 2.08,
   mac: 99.64,
   type: 'sans-serif'
 },{
-  fontFamily: 'Gill Sans',
+  family: 'Gill Sans',
   win: 58.54,
   mac: 95.5,
   type: 'sans-serif'
 },{
-  fontFamily: 'Helvetica',
+  family: 'Helvetica',
   win: 7.34,
   mac: 100,
   type: 'sans-serif'
 },{
-  fontFamily: 'Impact',
+  family: 'Impact',
   win: 0,
   mac: 95.14,
   type: 'sans-serif'
 },{
-  fontFamily: 'Lucida Grande',
+  family: 'Lucida Grande',
   win: 0,
   mac: 100,
   type: 'sans-serif'
 },{
-  fontFamily: 'Optima',
+  family: 'Optima',
   win: 2.52,
   mac: 93.69,
   type: 'sans-serif'
 },{
-  fontFamily: 'Segoe UI',
+  family: 'Segoe UI',
   win: 75.36,
   mac: 0,
   type: 'sans-serif'
 },{
-  fontFamily: 'Tahoma',
+  family: 'Tahoma',
   win: 99.95,
   mac: 91.71,
   type: 'sans-serif'
 },{
-  fontFamily: 'Trebuchet MS',
+  family: 'Trebuchet MS',
   win: 99.67,
   mac: 97.12,
   type: 'sans-serif'
 },{
-  fontFamily: 'Verdana',
+  family: 'Verdana',
   win: 99.84,
   mac: 99.1,
   type: 'sans-serif'
 },{
-  fontFamily: 'Big Caslon',
+  family: 'Big Caslon',
   win: 0,
   mac: 92.61,
   type: 'serif'
 },{
-  fontFamily: 'Bodoni MT',
+  family: 'Bodoni MT',
   win: 55.81,
   mac: 0,
   type: 'serif'
 },{
-  fontFamily: 'Book Antiqua',
+  family: 'Book Antiqua',
   win: 86.09,
   mac: 49.01,
   type: 'serif'
 },{
-  fontFamily: 'Calisto MT',
+  family: 'Calisto MT',
   win: 58.43,
   mac: 46.31,
   type: 'serif'
 },{
-  fontFamily: 'Cambria',
+  family: 'Cambria',
   win: 83.35,
   mac: 35.32,
   type: 'serif'
 },{
-  fontFamily: 'Didot',
+  family: 'Didot',
   win: 0,
   mac: 93.51,
   type: 'serif'
 },{
-  fontFamily: 'Garamond',
+  family: 'Garamond',
   win: 86.47,
   mac: 49.91,
   type: 'serif'
 },{
-  fontFamily: 'Georgia',
+  family: 'Georgia',
   win: 99.4,
   mac: 97.48,
   type: 'serif'
 },{
-  fontFamily: 'Goudy Old Style',
+  family: 'Goudy Old Style',
   win: 58.11,
   mac: 47.57,
   type: 'serif'
 },{
-  fontFamily: 'Hoefler Text',
+  family: 'Hoefler Text',
   win: 0.99,
   mac: 92.61,
   type: 'serif'
 },{
-  fontFamily: 'Lucida Bright',
+  family: 'Lucida Bright',
   win: 76.12,
   mac: 99.64,
   type: 'serif'
 },{
-  fontFamily: 'Palatino',
+  family: 'Palatino',
   win: 99.29,
   mac: 86.13,
   type: 'serif'
 },{
-  fontFamily: 'Perpetua',
+  family: 'Perpetua',
   win: 66.54,
   mac: 0,
   type: 'serif'
 },{
-  fontFamily: 'Rockwell',
+  family: 'Rockwell',
   win: 65.94,
   mac: 0,
   type: 'serif'
 },{
-  fontFamily: 'Rockwell Extra Bold',
+  family: 'Rockwell Extra Bold',
   win: 66.1,
   mac: 0,
   type: 'serif'
 },{
-  fontFamily: 'Baskerville',
+  family: 'Baskerville',
   win: 60.35,
   mac: 93.33,
   type: 'serif'
 },{
-  fontFamily: 'Times New Roman',
+  family: 'Times New Roman',
   win: 99.67,
   mac: 97.48,
   type: 'serif'
 },{
-  fontFamily: 'Consolas',
+  family: 'Consolas',
   win: 82.97,
   mac: 34.77,
   type: 'monospaced'
 },{
-  fontFamily: 'Courier New',
+  family: 'Courier New',
   win: 99.73,
   mac: 95.68,
   type: 'monospaced'
 },{
-  fontFamily: 'Lucida Console',
+  family: 'Lucida Console',
   win: 99.18,
   mac: 0,
   type: 'monospaced'
 },{
-  fontFamily: 'Lucida Sans Typewriter',
+  family: 'Lucida Sans Typewriter',
   win: 74.81,
   mac: 99.64,
   type: 'monospaced'
 },{
-  fontFamily: 'Monaco',
+  family: 'Monaco',
   win: 2.74,
   mac: 99.82,
   type: 'monospaced'
 },{
-  fontFamily: 'Andale Mono',
+  family: 'Andale Mono',
   win: 4.16,
   mac: 94.59,
   type: 'monospaced'
 },{
-  fontFamily: 'Copperplate',
+  family: 'Copperplate',
   win: 66.87,
   mac: 92.61,
   type: 'fantasy'
 },{
-  fontFamily: 'Papyrus',
+  family: 'Papyrus',
   win: 70.37,
   mac: 92.43,
   type: 'fantasy'
 },{
-  fontFamily: 'Brush Script MT',
+  family: 'Brush Script MT',
   win: 59.64,
   mac: 90.99,
   type: 'script'
